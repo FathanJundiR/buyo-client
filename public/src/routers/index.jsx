7647927c852799354e6b096c1d026c99e964cc4c
FathanJundiR <fathanjundi43@gmail.com>
2024-08-09 13:12:44 +0700

    fixing public loader

--- public/src/routers/index.jsx
+++ public/src/routers/index.jsx
@@ -36,27 +36,6 @@ const router = createBrowserRouter([
   },
   {
     element: <BaseLayout />,
-    loader: () => {
-      if (!localStorage.access_token) {
-        Toastify({
-          text: "Please log in first",
-          duration: 2000,
-          newWindow: true,
-          close: true,
-          gravity: "bottom",
-          position: "right",
-          stopOnFocus: true,
-          style: {
-            background: "#EF4C54",
-            color: "#17202A",
-            boxShadow: "0 5px 10px black",
-            fontWeight: "bold",
-          },
-        }).showToast();
-        return redirect("/login");
-      }
-      return null;
-    },
     children: [
       {
         path: "/",
